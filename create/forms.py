0c02e81b04a2ced594669051bfa0895b68d6ab38
Henry80074 <3henrybooth3@gmail.com>
2021-09-08 21:12:41 +0100

    working with therapy programme forms

--- create/forms.py
+++ create/forms.py
@@ -1,4 +1,15 @@
 from django import forms
+from django.forms import ModelForm
+from main.models import TherapyActivity
 
 class TherapyProgramme(forms.Form):
-    activities = forms.CharField(label='Your name', max_length=100)
+    activities = forms.(label='Your name', max_length=100)
+    reps = forms.IntegerField(label="reps", max_length=3)
+    sets = forms.IntegerField(label="sets", max_length=3)
+
+class TherapyProgrammeForm(ModelForm):
+    class Meta:
+        model  =  TherapyActivity
+    activities = forms.(label='Your name', max_length=100)
+    reps = forms.IntegerField(label="reps", max_length=3)
+    sets = forms.IntegerField(label="sets", max_length=3)
